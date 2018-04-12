b7816f33e6c22e291f09acd255d8c5776e809566
Mateus Seenem Tavares <maseta.mateus@gmail.com>
2018-04-12 20:44:27 -0300

    quase lá

--- trabSOP.c
+++ trabSOP.c
@@ -9,10 +9,13 @@
 
 #define MIN(a,b) ((a) < (b) ? a : b)
 #define TAMNOMEMAX 100
+#define QTD_MUTEX 2
 
 int exitFlag;
 int threadsOcupadas;
-pthread_mutex_t lock;
+pthread_mutex_t lock[QTD_MUTEX];
+//Barreira0 trava as threads em uma determinada região até que todas apotem para o primeiro elemento da lista vendedor
+pthread_barrier_t barreira0;
 
 //Estrutura da oferta de compra e venda
 typedef struct {
@@ -41,15 +44,6 @@ argThread * newArgThread(int nroThread, char * nomeArq) {
 char * nomeArquivo;
 LDDE * vendedor;
 
-/*
-    buscaOfertaCompra
-    Funcao que realiza compras
-    Tem como entrada
-    - ponteiro da lista de ofertas do comprador
-    - nome do produto que esta a venda
-    - quantidade disponivel para venda
-*/
-
 int quantidadeNode(NoLDDE * tmpNo) {
     return (*(info *)tmpNo->dados).quantidade;
 }
@@ -58,58 +52,12 @@ char * nomeNode(NoLDDE * tmpNo) {
     return (*(info *)tmpNo->dados).nome;
 }
 
-int buscaOfertaCompra(LDDE *p, char nome[], int quantDispVendedor){
-    NoLDDE *aux;
-    aux = p->inicioLista;
-    info *reg, x;
-    reg = &x;
-    int quantidadeComprada;
-    //Percorre lista comprador
-    while(aux->prox!=NULL) {
-        //obtem cada oferta do comprador
-        memcpy(reg, aux->dados, p->tamInfo);
-        //se quantidade é zero então o comprador está satisfeito
-        if(reg->quantidade == 0){
-            //printf("satifeito\n");
-            return 0;
-        }
-        //Compara o nome do produto da lista do vendedor com o nome da lista
-        // do comprador
-        if(memcmp(reg->nome,nome,sizeof(reg->nome))){
-            //Se iguais quer dizer que o comprador quer comprar algo com aquele nome
-            printf("encontrado\n");
-            //Se o vendedor tem uma quantidade maior que o comprador deseja
-            //o comprador compra todas daquele produto
-            if(quantDispVendedor >= reg->quantidade){
-                //comprador fica satisfeito em relação aquele produto
-                quantidadeComprada = reg->quantidade;
-                reg->quantidade = 0;
-                //registra na lista do vendedor o valor atualizado
-                memcpy(aux->dados, reg, p->tamInfo);
-                //retorna a quatidade comprada
-                return quantidadeComprada;
-            }else{
-                //Se o vendedor tem menos que o comprador precisa
-                reg->quantidade = reg->quantidade - quantDispVendedor;
-                memcpy(aux->dados, reg, p->tamInfo);
-                quantidadeComprada = quantDispVendedor;
-                return quantidadeComprada;
-            }
-
-        }
-        aux = aux->prox;
-    }
-  return 0;
-}
-
-
 int quantidadeCompra(NoLDDE * ptrVendedor, NoLDDE * ptrOferta) {
     int qtdVenda  = quantidadeNode(ptrVendedor);
     int qtdCompra = quantidadeNode(ptrOferta);
 
     return MIN(qtdVenda, qtdCompra);
 }
-
 
 void * corretor(void *argumentos){
 	LDDE * listaOfertas = listaCriar(sizeof(info));
@@ -130,11 +78,11 @@ void * corretor(void *argumentos){
         	fclose(ptr);
         }
         //local de disputa pela variavel thread ocupadas
-        pthread_mutex_lock(&lock);
+        pthread_mutex_lock(&lock[0]);
         threadsOcupadas = threadsOcupadas-1;
         printf("[%s] leu arquivo\n", args->nomeArq);
         NoLDDE * temp = listaOfertas->inicioLista;
-        pthread_mutex_unlock(&lock);
+        pthread_mutex_unlock(&lock[0]);
 
         //local de verificar ofertas de vendas e realizar a compra
         //Codigo apenas para testes
@@ -145,31 +93,41 @@ void * corretor(void *argumentos){
             ptrVendedor = vendedor->inicioLista;
             if(ptrVendedor != NULL) break;
         }
-
         printf("[%s] PtrVendedor deixou de ser NULL\n", args->nomeArq);
+        pthread_barrier_wait(&barreira0);      
 
         while(1) {
             ptrOferta = listaOfertas->inicioLista;
             while(ptrOferta != NULL) {
                 if(strcmp(nomeNode(ptrVendedor), nomeNode(ptrOferta)) == 0) {
-                    pthread_mutex_lock(&lock);
+                    pthread_mutex_lock(&lock[1]);
                     if(quantidadeNode(ptrVendedor) > 0 && quantidadeNode(ptrOferta) > 0) {
                         int qtdCompra = quantidadeCompra(ptrVendedor, ptrOferta);
                         printf("[%s] Comprando %d de %s\n", args->nomeArq, qtdCompra, nomeNode(ptrVendedor));
                         (*(info *)ptrVendedor->dados).quantidade -= qtdCompra;
                         (*(info *)ptrOferta->dados).quantidade   -= qtdCompra;
                     }
-                    pthread_mutex_unlock(&lock);
+                    pthread_mutex_unlock(&lock[1]);
                 }
                 ptrOferta = ptrOferta->prox;
             }
 
-            if(ptrVendedor == vendedor->fimLista && exitFlag) { //ponteiro chegou ao fim da lista e não há mais itens para serem inseridos
+            if(ptrVendedor == vendedor->fimLista && exitFlag) { 
+                //ponteiro chegou ao fim da lista e não há mais itens para serem inseridos
+                pthread_mutex_lock(&lock[2]);
                 printf("[%s] ptrVendedor chegou ao fim\n", args->nomeArq);
+                ptrOferta = listaOfertas->inicioLista;
+                while(ptrOferta != NULL) {
+                    printf("%s %d\n", nomeNode(ptrOferta), quantidadeNode(ptrOferta));
+                    ptrOferta = ptrOferta->prox;
+                }
+                pthread_mutex_unlock(&lock[2]);
                 pthread_exit(NULL);
-            } else if(ptrVendedor == vendedor->fimLista && !exitFlag) //ponteiro cheogu ao fim da lista e ainda há itens a serem inseridos
+            } else if(ptrVendedor == vendedor->fimLista && !exitFlag) 
+                //ponteiro cheogu ao fim da lista e ainda há itens a serem inseridos, ele só continua esperando
                 continue;
-            else { //ponteiro não chegou ao fim da lista então ele pode avançar
+            else { 
+                //ponteiro não chegou ao fim da lista então ele pode avançar
                 ptrVendedor = ptrVendedor->prox;
             }
         }
@@ -209,15 +167,18 @@ int main(int argc, char** argv) {
 	int i;
     vendedor = listaCriar(sizeof(info));
 
-    if(pthread_mutex_init(&lock, NULL) != 0) {
-        fprintf(stderr, "Erro na criação do Mutex, amigo se chegou aqui você é um campeão");
-        exit(EXIT_FAILURE);
-    }
+    for(i = 0; i < QTD_MUTEX; i++) {
+        if(pthread_mutex_init(&lock[i], NULL) != 0) {
+            fprintf(stderr, "Erro na criação do Mutex, amigo se chegou aqui você é um campeão");
+            exit(EXIT_FAILURE);
+        }
+    }   
 
 	//Argumentos passados pela linha de comando
     qtdThreads = atoi(argv[1]);
     threadsOcupadas = qtdThreads;
     pthread_t threadCorretor[qtdThreads];
+    pthread_barrier_init(&barreira0, NULL, qtdThreads);
 
 	//Cria threads corretores
 	for(i = 0; i < qtdThreads; i++){
@@ -233,6 +194,7 @@ int main(int argc, char** argv) {
     }
 
     printf("[ Main ] Todos os arquivos foram lidos\n\n");
+
 	FILE * ptr = fopen(argv[2],"r");
 	if(vendedor != NULL) {
         if(ptr){
